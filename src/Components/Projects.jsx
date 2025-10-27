7f2f6a11a8acd17f82b6ffd90dbc8ad2e9a1e4f4
Mohammad Khurram <mohdkaif99177@gmail.com>
2025-10-27 22:24:42 +0530

    Update img02 URL with correct Cloudinary path

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -9,7 +9,7 @@ const projectsData = {
   architecture: {
     images: [
       "https://res.cloudinary.com/dy31puega/image/upload/v1761579852/img01_hqyjn5.webp",
-      "https://res.cloudinary.com/dy31puega/image/upload/v1761579852/img02_hqyjn5.webp",
+      "https://res.cloudinary.com/dy31puega/image/upload/v1761579853/img02_ogmxgf.webp",
       "https://res.cloudinary.com/dy31puega/image/upload/v1761579852/img03_hqyjn5.webp",
       "https://res.cloudinary.com/dy31puega/image/upload/v1761579854/img04_oanhad.webp",
       "https://res.cloudinary.com/dy31puega/image/upload/v1761579856/img05_ycc7wx.webp",
